ad3fd18e433e316be2cdcd892c3d038eeb6f6608
Vance <vapttheman@gmail.com>
2024-11-30 14:28:39 -0800

    Removed Unnessecary Assets

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -17,6 +17,5 @@ func _on_button_pressed() -> void:
 func _on_level_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/levels_menu.tscn")
 
-
 func _on_credits_pressed() -> void:
 		get_tree().change_scene_to_file("res://Scenes/credits.tscn")
